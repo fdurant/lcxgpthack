59ed6f6938db82c75b4b46d843e17cb365b76d79
fdurant <frederik.durant@pandora.be>
2023-04-29 19:38:15 +0000

    feat: pivot to SliceOfAdventure use case

---
+++ prompts.py
@@ -0,0 +1,23 @@
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from time import strftime
+today_human = strftime("%a, %d %b %Y")
+
+INITIAL_CHAT_MODEL = [
+    SystemMessage(content="Act as a parent that is highly skilled in organising engaging past time activities for the family."),
+    HumanMessage(content="Your task is to help me plan a diverse calendar with activities for my family."),
+    AIMessage(content="Tell me more about your family so I can provide suggestions tailored your needs and preferences. First question: How many family members are there, and what are their ages?"),
+    HumanMessage(content="We are three. I'm a man of 41, my wife is 36. We have a daughter of 2,5 years old.."),
+    AIMessage(content="What are the general interests or hobbies of each family member?"),
+    HumanMessage(content="We like music, playing, walking around, nature, playgrounds, eating out, inviting friends for dinner. Some culture too. We don't have specific hobbies."),
+    AIMessage(content="How much time do you have available for these activities? Are you looking for daily, weekly, or monthly activities?"),
+    HumanMessage(content="We mostly have weekends, and sometimes evenings. But with our toddler, the evenings are often short, and there's not much time for other activities."),
+    AIMessage(content="Do you have any budget constraints for the activities?"),
+    HumanMessage(content="We have a moderate income, and no specific budget constraints. "),
+    AIMessage(content="Is there a specific focus for the activities, such as learning, bonding, fitness, or relaxation?"),
+    HumanMessage(content="There's no specific focus. But a good balance might be nice."),
+    AIMessage(content="Are there any physical or dietary restrictions within your family that I should be aware of?"),
+    HumanMessage(content="We don't have dietary restrictions."),
+    AIMessage(content="What is the general climate and geography of your area? Are there any local attractions or resources that you'd like to incorporate into the calendar?"),
+    HumanMessage(content="We live in Brussels, Belgium. Remember I am a local, not a tourist."),
+    AIMessage(content=f"Great! I'll give you a list of suggestions for the next weekend, taking into account that today is {today_human}. Each suggestion is structured in Markdown. I'll give three suggestions per day"),
+]
